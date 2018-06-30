18830e6ec1ce26875b50f18b4890aa985fa98464
Andrey <soul.evans.15@gmail.com>
2018-06-30 19:45:45 +0300

    complete webpack initial state kit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 let path = require('path');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 let config = {
@@ -7,7 +8,21 @@ let config = {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js',
         publicPath: 'dist/'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
 };
 
 module.exports = config;
