8c769a12c9458c9f13a977df8b02406bc99a17cb
Andrey <soul.evans.15@gmail.com>
2018-06-30 19:07:43 +0300

    initial state for project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+let path = require('path');
+
+
+let config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    devServer: {
+        overlay: true
+    }
+
+};
+
+module.exports = config;
